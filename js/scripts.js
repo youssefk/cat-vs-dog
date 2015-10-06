8e41005a1db55484c86178adc6ad1ef81d160bbe
Youssef <youssefkhalifa@live.com>
2015-10-05 19:09:03 -0700

    Roll back image implementation, it broke the page upon li removal. Fix small bug with removing alternating li items.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
 $(document).ready(function(){
   $("button#meow").click(function(){
-    $("ul#cat").prepend("<img src='img/cat.jpg' class='profile'>");
     $("ul#cat").prepend("<li>Meow</li>");
-    $("ul#dog").prepend("<img src='img/dog.jpg' class='profile'>");
     $("ul#dog").prepend("<li>Bark</li>");
+
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
+
   });
 
   $("button#bark").click(function(){
-    $("ul#cat").prepend("<img src='img/cat-hiss.jpg' class='profile'>");
-    $("ul#cat").prepend("<li>Hiss...</li>");
-    $("ul#dog").prepend("<img src='img/dog-sad.jpg' class='profile'>");
     $("ul#dog").prepend("<li>Whimper</li>");
+    $("ul#cat").prepend("<li>Hiss...</li>");
+
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
+
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+
   });
 
 });
